2b1faafae29dc15393c599c6e5a0b77c711dce1c
Tomer Biton <168045524+TomerBitonn@users.noreply.github.com>
2024-06-23 01:37:11 +0300

    Update Account.h

--- BankAccount/Account.h
+++ BankAccount/Account.h
@@ -5,19 +5,19 @@ using namespace std;
 #pragma once
 class Account
 {
-	friend ostream& operator<<(ostream& output, const Account& customer); // operator overloading
+	friend ostream& operator<<(ostream& output, const Account& customer); 
 private:
 	char name[30];
 	double money;
 	int accountId;
 	static int accountIdGenerator;
-	Account* next; // pointer to the next object using linked list
+	Account* next; 
 public:
 	Account() : name(), money(),accountId(),next(NULL) {};
-	Account(const char* name, double money = 0); // default values - right to left
-	char* GetName() const; // return a pointer to the beggining of the array
-	void SetName(const char* new_name); // in "set" functions work with new names for the variables
-	void SetNext(Account &next_account); // recieve a reference to an Account object
+	Account(const char* name, double money = 0); 
+	char* GetName() const; 
+	void SetName(const char* new_name); 
+	void SetNext(Account &next_account); 
 	Account* GetNext() const;
 	void print() const;
 };
