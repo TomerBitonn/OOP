0627115e32a5f7e892bdb05ec827ac6fa721187c
Tomer Biton <168045524+TomerBitonn@users.noreply.github.com>
2024-06-23 01:43:19 +0300

    Update Rational.h

--- RationalNumbers/Rational.h
+++ RationalNumbers/Rational.h
@@ -4,7 +4,7 @@ using namespace std;
 #pragma once
 class Rational
 {
-	friend istream& operator>>(istream& input, Rational& r); // friend function cant be const
+	friend istream& operator>>(istream& input, Rational& r);
 	friend ostream& operator<<(ostream& output, const Rational& r);
 	friend Rational& operator+(const int& num,const Rational& r);
 	friend Rational& operator*(const int& num, const Rational& r);
@@ -14,13 +14,13 @@ private:
 	int numerator;
 	int denominator;
 public:
-	Rational(int mone = 0, int mechane = 1);// recommended not to work with the same names in the constructor
+	Rational(int mone = 0, int mechane = 1);
 	void Gcd();
 	void SetNumerator(int mone);
 	void SetDenomirator(int mechane);
 	int GetNumerator() const;
 	int GetDenominator() const;
-	Rational& operator+(const Rational& r) const; // rational + rational. doesnt change the values, return a new one.
+	Rational& operator+(const Rational& r) const; 
 	Rational& operator+(const int& num) const;
 	Rational& operator*(const Rational& r) const;
 	Rational& operator*(const int& num) const;
